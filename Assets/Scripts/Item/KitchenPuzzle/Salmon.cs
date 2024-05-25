af34fefe8043722b259c5a8f6980df5f17f42656
ruring <76811232+rururing@users.noreply.github.com>
2024-05-25 16:00:10 +0900

    stage 3 상호작용 추가

--- Assets/Scripts/Item/KitchenPuzzle/Salmon.cs
+++ Assets/Scripts/Item/KitchenPuzzle/Salmon.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Salmon : MonoBehaviour
+public class Salmon : InteractiveItem
 {
-    // Start is called before the first frame update
+    public Text lockedText;
+
     void Start()
     {
-        
+        lockedText.gameObject.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void onClick()
     {
         
+         lockedText.gameObject.SetActive(true);
+         StartCoroutine(DisableTextAfterDelay(1.0f));
+     
+
+    }
+    private IEnumerator DisableTextAfterDelay(float delay)
+    {
+        
+        yield return new WaitForSeconds(delay);
+        
+        lockedText.gameObject.SetActive(false);
+        pickUp();
+        // pickUp 호출
+
+    }
+
+    public override void pickUp()
+    {
+        // 인벤토리에 데이터 넣고 
+        Destroy(this.gameObject);
+
     }
 }
